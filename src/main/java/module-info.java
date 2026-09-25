a49390e37b612cb1b635b967e6f23bb4f9239a99
JostinApaza <jostinapaza@gmail.com>
2026-09-24 21:31:34 -0500

    test commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.example.test1 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+
+    opens com.example.test1 to javafx.fxml;
+    exports com.example.test1;
+}
